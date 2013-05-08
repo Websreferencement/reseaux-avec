9e40a3a15528a42c83220bd10a0e5499cf2a290b
David Jegat <david.jegat@gmail.com>
2013-05-09 00:48:40 +0200

    I have just missed to add the menu entity :(

---
+++ src/App/Entity/Menu.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="menu")
+ */
+class Menu
+{
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
+
+	/**
+	 * @ORM\Column(type="string", length=200)
+	 */
+	private $name;
+
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $route;
+
+	/**
+	 * @ORM\OneToMany(targetEntity="Menu", mappedBy="parent")
+	 */
+	private $childs;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity="Menu", inversedBy="children")
+	 */
+	private $parent;
+
+	/**
+	 * Get id
+	 * 
+	 * @return integer
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+	
+	/**
+	 * Set id
+	 *
+	 * @param integer $id
+	 * @return Menu
+	 */
+	public function setId($id)
+	{
+		$this->id = $id;
+	
+		return $this;
+	}
+
+	/**
+	 * Get name
+	 * 
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+	
+	/**
+	 * Set name
+	 *
+	 * @param string $name
+	 * @return Menu
+	 */
+	public function setName($name)
+	{
+		$this->name = $name;
+	
+		return $this;
+	}
+
+	/**
+	 * Get route
+	 * 
+	 * @return string
+	 */
+	public function getRoute()
+	{
+		return $this->route;
+	}
+	
+	/**
+	 * Set route
+	 *
+	 * @param string $route
+	 * @return Menu
+	 */
+	public function setRoute($route)
+	{
+		$this->route = $route;
+	
+		return $this;
+	}
+
+	/**
+	 * Get childs
+	 * 
+	 * @return ArrayCollection
+	 */
+	public function getChilds()
+	{
+		return $this->childs;
+	}
+	
+	/**
+	 * add children
+	 *
+	 * @param Menu $children
+	 * @return childs
+	 */
+	public function addChildren($children)
+	{
+		$this->childs[] = $children;
+	
+		return $this;
+	}
+	
+	/**
+	 * has children
+	 *
+	 * @param Menu $children
+	 * @return boolean
+	 */
+	public function hasChildren($children)
+	{
+		foreach($this->childs as $item){
+			if($item === $children){
+	
+				return true;
+			}
+		}
+	
+		return false;
+	}
+
+	public function __construct()
+	{
+		$this->childs = new ArrayCollection();
+	}
+
+}
