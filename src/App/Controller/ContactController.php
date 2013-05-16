bb41e868187e97953109accb189931a5dea761b3
davidjegat <david.jegat@gmail.com>
2013-05-16 17:39:40 +0200

    Add contact management to the backend system.

---
+++ src/App/Controller/ContactController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use Knp\RadBundle\Controller\Controller;
+use App\Entity\Contact;
+
+class ContactController extends Controller
+{
+
+	public function indexAction()
+	{
+		$contacts = $this->getRepository('App:Contact')
+			->findAll();
+
+		return array(
+			'contacts' => $contacts
+		);
+	}
+
+	public function newAction(Request $request)
+	{
+		$contact = new Contact();
+
+		$form = $this->createBoundObjectForm($contact, 'new');
+
+		if($form->isBound() and $form->isValid()){
+			$this->persist($contact);
+			$this->flush();
+
+			$this->addFlash('info', 'Le contact '.$contact.' créé avec succés !');
+
+			return $this->redirectToRoute('app_contact_index');
+		}
+
+		return array(
+			'form' => $form->createView()
+		);
+	}
+
+	public function editAction($id)
+	{
+		$contact = $this->findOr404('App:Contact', array('id' => $id));
+
+		$form = $this->createBoundObjectForm($contact, 'edit');
+
+		if($form->isBound() and $form->isValid()){
+			$this->persist($contact);
+			$this->flush();
+
+			$this->addFlash('info', 'Le contact '.$contact.' édité avec succés !');
+
+			return $this->redirectToRoute('app_contact_index');
+		}
+
+		return array(
+			'form' => $form->createView(),
+			'data' => $contact
+		);
+	}
+
+	public function deleteAction($id)
+	{
+		$contact = $this->findOr404('App:Contact', array('id' => $id));
+
+		$this->remove($contact);
+		$this->flush();
+
+		$this->addFlash('info', 'Contact '.$contact.' suprimé avec succés !');
+
+		return $this->redirectToRoute('app_contact_index');
+	}
+}
--- src/App/Entity/Contact.php
+++ src/App/Entity/Contact.php
@@ -136,4 +136,9 @@ class Contact
 	
 		return $this;
 	}
+
+	public function __toString()
+	{
+		return $this->getName();
+	}
 }
