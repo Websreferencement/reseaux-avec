2239aeba4137bc941e5e454a3780f69ad760fb20
David Jegat <david.jegat@gmail.com>
2013-06-02 21:12:29 +0200

    Add navigation sidebar support

--- src/App/Sidebar/NavigationSidebar.php
+++ src/App/Sidebar/NavigationSidebar.php
@@ -19,6 +19,17 @@ class NavigationSidebar extends AbstractWidget
 
 	public function initialize()
 	{
+        $actions = $this->container
+            ->get('app.entity.action_repository')
+            ->findBy(array(), null, 2);
+
+        $news = $this->container
+            ->get('app.entity.news_repository')
+           ->findBy(array(), null, 2); 
+
+        $this->args->set('news', $news);
+        $this->args->set('actions', $actions);
+
 		return;
 	}
 }
